d807a32cdd96d61956956c1b98125ebadd3d2319
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-04-20 14:36:22 -0700

    working on modules

---
+++ models/Thought.js
@@ -0,0 +1,25 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            require: true,
+            maxLength: 280
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
