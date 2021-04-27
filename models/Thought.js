31ed22b04d2e27338327ebcb512b699f766c1480
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-04-27 14:38:56 -0700

    finished models and utils

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const ReactionSchema = new Schema(
     {
@@ -24,4 +25,39 @@ const ReactionSchema = new Schema(
     }
 );
 
-// create a dateFormat
+// Thought schema section
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema],
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
